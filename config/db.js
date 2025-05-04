37bb4627a763f1d444e585c00b2e49561b6248a5
Developer-Linus <linusmksu30@gmail.com>
2025-05-04 18:09:30 +0300

    Created MySQL pool connection for concurrent users and wrote SQL queries for users

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// Responsible for creating and exporting MySQL pool connection
+
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+// Create a MySQL pool connection for efficient query management
+const pool = mysql.createPool(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    }
+);
+
+export default pool;
+
---
+++ models/user_model.js
@@ -0,0 +1,18 @@
+// Handles database queries related to users
+
+import pool from "../config/db.js";
+
+export async function createUser({ first_name, last_name, email, password }){
+    const sql = "INSERT INTO users (first_name, last_name, email, password_hash) VALUES (?, ?, ?, ?)";
+
+    // Result is an array containing the metadata of query execution
+    const [ result ] = await pool.execute(sql, [first_name, last_name, email, password]);
+    return result.insertId; // Return new user's ID
+}
+
+export async function findUserByEmail(email){
+    const sql = "SELECT * FROM users WHERE email=? LIMIT = 1";
+
+    const [ rows ] = await pool.execute(sql, [email]);
+    return rows[0];
+}
