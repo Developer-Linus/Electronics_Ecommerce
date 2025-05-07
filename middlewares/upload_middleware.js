a7cc69d0348138c0795efa4c85d3bb9f8cb136cb
Developer-Linus <linusmksu30@gmail.com>
2025-05-07 10:30:12 +0300

    Multer bug fix: fixed a multer bug to accomodate recommended  file types.

--- middlewares/upload_middleware.js
+++ middlewares/upload_middleware.js
@@ -1,29 +1,26 @@
 import multer from "multer";
-import path, { extname } from "path";
-
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-// Required to get __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path";
 
 // Storage engine
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-    cb(null, "uploads/")}, // Save files in uploads/ folder
-
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix+path.extname(file.originalname));
-    }
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Save files in uploads/ folder
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
 });
 
-// file filter
+// File filter
 const fileFilter = (req, file, cb) => {
-    const allowed = [ 'image/jpg', 'image/png', 'image/webp' ];
-    cb(null, allowed.includes(file.mimetype));
-}
+  const allowed = ['image/jpeg', 'image/png', 'image/webp', 'image/jpg'];
+  if (allowed.includes(file.mimetype)) {
+    cb(null, true); // Accept the file
+  } else {
+    cb(new Error("Only .jpg, .jpeg, .png, and .webp formats allowed"), false);
+  }
+};
 
 // Export the configured multer middleware
 const upload = multer({ storage, fileFilter });
